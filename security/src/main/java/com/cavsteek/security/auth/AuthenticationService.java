f025c23c432177e8e27764ee67d3606006adace1
Ikemdinachi Uzochukwu <uzochukwuikemdinachi@gmail.com>
2023-11-20 22:43:14 +0100

    6th commit

---
+++ security/src/main/java/com/cavsteek/security/auth/AuthenticationService.java
@@ -0,0 +1,36 @@
+package com.cavsteek.security.auth;
+
+import com.cavsteek.security.Repo.UserRepo;
+import com.cavsteek.security.config.JwtService;
+import com.cavsteek.security.user.Role;
+import com.cavsteek.security.user.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+    private final UserRepo repository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        repository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        return null;
+    }
+}
