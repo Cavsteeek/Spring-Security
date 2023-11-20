06155f6bd17a3e35525775148d2c19c476c8f46a
Ikemdinachi Uzochukwu <uzochukwuikemdinachi@gmail.com>
2023-11-20 21:16:30 +0100

    6th commit

--- security/src/main/java/com/cavsteek/security/config/JwtAuthenticationFilter.java
+++ security/src/main/java/com/cavsteek/security/config/JwtAuthenticationFilter.java
@@ -9,6 +9,7 @@ import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -19,6 +20,7 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final JwtService jwtService;
+    private UserDetailsService userDetailsService;
     @Override
     protected void doFilterInternal(
            @NonNull HttpServletRequest request,
@@ -38,7 +40,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
             if (jwtService.isTokenValid(jwt, userDetails)){
                 UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
-                        userDetails,
+                         userDetails,
                         null,
                         userDetails.getAuthorities()
                 );
@@ -52,4 +54,4 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     }
 
     }
-}
+
